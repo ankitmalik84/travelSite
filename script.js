9488b3ddc3289886f87b573718f9e202cee66f75
Ankit Malik <ankitmalik844903@gmail.com>
2023-11-29 20:09:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const header = document.querySelector("[data-header]");
+
+navToggleBtn.addEventListener("click", function () {
+  this.classList.toggle("active");
+  header.classList.toggle("active");
+});
+
+/**
+ * show go top btn when scroll window to 500px
+ */
+
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+  window.scrollY >= 500
+    ? goTopBtn.classList.add("active")
+    : goTopBtn.classList.remove("active");
+});
+
 // Firbase Authentication of Email/Password Started
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
